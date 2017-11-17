c8ead4b7d9ad6b16330ec692018509d111c4d515
NuSa <lafon.eddy@gmail.com>
2017-11-17 14:49:39 +0100

    Create InputManager singleton

--- Assets/Scripts/InputManager/InputManager.cs
+++ Assets/Scripts/InputManager/InputManager.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour {
 
+    private static InputManager myInstance;
+
 	// Use this for initialization
 	void Start () {
-		
+        myInstance = this;
+
+        DontDestroyOnLoad(this);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public static InputManager get_Instance()
+    {
+        if (myInstance != null)
+        {
+#if UNITY_EDITOR
+            Debug.Log("Return InputManager.myInstance");
+#endif
+            return myInstance;
+        }
+        else
+        {
+#if UNITY_EDITOR
+            Debug.Log("Return InputManager.myInstance = new InputManager();");
+#endif
+            return myInstance = new InputManager();
+        }
+    }
 }
